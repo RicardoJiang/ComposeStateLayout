67e36503530adfe8e7213ffcb9b8cd550cc0268c
shenzhen2017 <2868405029@qq.com>
2021-09-17 07:42:54 +0800

    ADD: init project

---
+++ app/src/main/java/com/zj/composestate/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.zj.composestate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.zj.composestate.ui.theme.ComposeStateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeStateTheme {
+        Greeting("Android")
+    }
+}
